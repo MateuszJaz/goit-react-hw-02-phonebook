1dd7088398a3cdad2b6f5555fd98f10f897c0b42
MateuszJaz <mateusz.jaz@outlook.com>
2023-01-04 17:11:30 +0100

    second commit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,35 +2,13 @@ import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
 import style from './ContactForm.module.css';
 
-const INITIAL_STATE = {
-  name: '',
-  number: '',
-};
-
 class ContactForm extends Component {
-  state = { ...INITIAL_STATE };
   id = nanoid();
 
-  handleInputChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.handleReset();
-  };
-
-  handleReset = () => {
-    this.setState({ ...INITIAL_STATE });
-  };
-
   render() {
-    const { name, number } = this.state;
     return (
       <>
-        <form className={style.form} onSubmit={this.handleSubmit}>
+        <form className={style.form} onSubmit={this.props.onSubmit}>
           <label htmlFor={this.id}>
             Name
             <input
@@ -40,8 +18,7 @@ class ContactForm extends Component {
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              value={name}
-              onChange={this.handleInputChange}
+              onChange={this.props.handleInputChange}
               placeholder="e.g. John Doe"
               required
             />
@@ -55,7 +32,6 @@ class ContactForm extends Component {
               name="number"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              value={number}
               onChange={this.handleInputChange}
               placeholder="e.g. 123-456-789"
               required

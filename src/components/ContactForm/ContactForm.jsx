8b450817da618db725fefa59796f1bfc569f19e3
MateuszJaz <mateusz.jaz@outlook.com>
2023-01-05 12:07:27 +0100

    cleanup

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -10,7 +10,7 @@ class ContactForm extends Component {
     return (
       <>
         <form className={style.form} onSubmit={this.props.onSubmit}>
-          <label htmlFor={this.id}>
+          <label htmlFor={'name'}>
             Name
             <input
               className={style.formInput}
@@ -19,12 +19,11 @@ class ContactForm extends Component {
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              onChange={this.props.handleInputChange}
               placeholder="e.g. John Doe"
               required
             />
           </label>
-          <label htmlFor={this.id}>
+          <label htmlFor={'number'}>
             Number
             <input
               className={style.formInput}
@@ -33,7 +32,6 @@ class ContactForm extends Component {
               name="number"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              onChange={this.handleInputChange}
               placeholder="e.g. 123-456-789"
               required
             />
